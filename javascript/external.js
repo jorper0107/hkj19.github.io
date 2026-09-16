dc0e4f39409287f5e896c4c9b2c0b750709e339a
jorper0107 <hkj19@txstate.edu>
2026-09-16 15:35:32 -0500

    Saved username

--- javascript/external.js
+++ javascript/external.js
@@ -10,8 +10,5 @@
 // A prompt box is used to prompt users to input a value before entering a page.
 //user_name = window.prompt("Please enter your name", "Type your name here");
 //document.write(user_name);
-arr = ["Mael","Eric","Randall","James"];
-for (var i=0; i < arr.length; i++)
-{
-document.writeln(arr[i],"<br>");
-}
+let user_name = window.prompt("Please enter your name:");
+document.write(user_name)
