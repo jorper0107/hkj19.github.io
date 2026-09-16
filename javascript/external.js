9a7e927cebfd3741f47601dc215daf75aac343e0
jorper0107 <hkj19@txstate.edu>
2026-09-16 14:43:22 -0500

    Completed Task 2 Output

--- javascript/external.js
+++ javascript/external.js
@@ -1,9 +1,5 @@
 // document.write("Where is my JS code?");
 // window.alert(5 + 6);
 //window.alert('5 + 6');
-//document.write('<h1>A heading</h1>');
-//document.write('<p>A sentence.</p>');
-const x = "web";
-const y = "mapping";
-const out = x + y;
-document.write(out);
+document.write('<h1>A heading</h1>');
+document.write('<p>A sentence.</p>');
